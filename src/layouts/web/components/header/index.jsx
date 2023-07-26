e851ff00b38106f9c01eaef203216d9b52a5879f
Mert <mertokan5020@gmail.com>
2023-07-26 18:40:03 +0300

    navbar css sorunu düzeltildi

--- src/layouts/web/components/header/index.jsx
+++ src/layouts/web/components/header/index.jsx
@@ -15,9 +15,10 @@ const Header = () => {
   const handleShow = () => {
     setShow(!show)
   }
+  console.log(window.innerWidth)
 
   const handleMenuScreen = () => {
-    if (window.innerWidth <= 639) {
+    if (window.innerWidth > 640) {
       setMenuScreen(true)
     } else {
       setMenuScreen(false)
@@ -37,10 +38,10 @@ const Header = () => {
   return (
     <nav
       className={classNames(
-        'flex fixed top-0 w-full left-0 justify-between h-16 py-2 items-center px-8 transition-all duration-300 lg:px-36',
+        'flex fixed top-0 w-full left-0 justify-between h-16 py-2 items-center px-8 transition-all duration-300 lg:px-36 ',
         {
           'h-12 bg-neutral-8/80': colorChange && !show,
-          'h-12 bg-neutral-8/95': show,
+          'h-12 bg-neutral-8/90': show,
           'bg-transparent': !colorChange,
         }
       )}
@@ -49,30 +50,31 @@ const Header = () => {
         <img src={logoDark} alt='' className='w-full h-full object-contain' />
       </Link>
       <div
-        className={classNames('sm:block transition-all duration-300', {
-          'absolute bg-neutral-8/95 top-16 left-0 w-full h-[calc(100vh-64px)]': show,
-          'absolute bg-neutral-8/95 sm:opacity-100 top-16 -left-[639px] w-full h-[calc(100vh-64px)]':
-            !show && menuScreen,
+        className={classNames('block transition-all duration-300', {
+          'absolute bg-neutral-8/90 top-16 left-0 w-full h-[calc(100vh-64px)]': show,
+          'absolute bg-neutral-8/90 opacity-0 sm:opacity-100 top-16 -left-[639px] w-full h-[calc(100vh-64px)]':
+            !show && !menuScreen,
         })}
       >
         <ul
           className={classNames('flex gap-12 text-white font-bold transition-all duration-300', {
-            'flex-col w-full gap-0 h-full justify-between': show,
+            'flex-col w-full gap-6 h-full': show,
           })}
         >
           {NAVBAR_MENU.map((menu, key) => (
             <NavLink
               key={key}
               to={menu.path}
+              onClick={() => setShow(false)}
               className={({isActive}) =>
                 classNames('relative transition-all duration-300 ease-in-out', {
                   'text-neutral-0 before:w-0 before:h-1 before:absolute before:-bottom-1 before:rounded-full before:right-0 before:bg-primary-0 before:transition-all before:duration-700 hover:before:w-full hover:before:left-0 hover:before:bg-state-orange hover:text-state-blue':
                     !isActive && !show,
                   'text-state-blue  after:content-[""] after:block after:absolute after:bg-state-orange after:h-1 after:w-full after:-bottom-1 after:left-0 after:rounded-full':
                     isActive && !show,
-                  'w-full self-center': show,
+                  'w-full text-center': show,
                   'text-neutral-0 hover:text-state-blue': !isActive && show,
-                  'text-state-blue': isActive && show,
+                  'text-primary-2 bg-state-blue': isActive && show,
                 })
               }
             >
